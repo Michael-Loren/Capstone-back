a112c676a2470ec94652d2670a9af7a338d264ff
Yongqi10 <q865984106@yahoo.com>
2022-01-24 15:00:08 -0500

    email and pssword check

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ app.use(express.json());
 app.use("/", require("./routes/login.js"));
 app.use("/", require("./routes/Register.js"));
 app.use('/profile',require('./routes/profile'))
+app.use('/',require('./routes/listOfFood'))
 const port = 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`))
