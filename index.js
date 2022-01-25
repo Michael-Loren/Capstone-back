841092e2712d5efde88cb92e238b2f985b438982
Yongqi10 <q865984106@yahoo.com>
2022-01-25 10:52:12 -0500

    list of food

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/", require("./routes/login.js"));
 app.use("/", require("./routes/Register.js"));
-app.use('/profile',require('./routes/profile'))
-app.use('/',require('./routes/listOfFood'))
+app.use("/profile", require("./routes/profile"));
+app.use("/", require("./routes/listOfFood"));
+
 const port = 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
